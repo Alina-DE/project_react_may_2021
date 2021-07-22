2c94a87e140388342518b588eba70145a62917fb
Alina <alirei@gmail.com>
2021-07-22 12:38:35 +0200

    minor corrections

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -13,7 +13,7 @@ export default function Weather() {
             <div className="Weather">
                 <div>
 
-                    {weather.main.temp > 0 ? '+' + Math.ceil(weather.main.temp) + "° C" : Math.floor(weather.main.temp) + "° C"}
+                    {(weather.main.temp > 0 ? "+" : "") + Math.ceil(weather.main.temp) + "° C"}
 
                     <img src={"http://openweathermap.org/img/wn/" + weather.weather[0].icon + ".png"} alt={weather.weather[0].description} />
                 </div>
